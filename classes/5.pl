bc45857136286a623bd1aad3ae13f527a2a4b047
up201504874@FEUPSIG.FE.UP.PT <up201504874@FEUPSIG.FE.UP.PT>
2018-04-06 12:11:36 +0100

    hillclimbing

--- classes/5.pl
+++ classes/5.pl
@@ -1,3 +1,5 @@
+:-use_module(library(lists)).
+:-use_module(library(between)).
 
 
 slots(4).
@@ -20,3 +22,45 @@ incompat(D1,D2,NA) :-
   disciplina(D2,LA2),
   findall(A,(member(A,LA1), member(A,LA2)),LA12),
   length(LA12,NA).
+ 
+ %  d1 d2 d3
+ % [1, 2, 3, 3,..] = L
+ 
+ f_aval(L,V) :-
+ findall(N,
+		(nth1(D1,L,Slot),
+		nth1(D2,L,Slot),
+		D1 < D2, %antes, D1 \= D2, < para eliminar simetrias
+		incompat(D1,D2,N)),  
+		LIncomp),
+		sumlist(LIncomp,V).
+		
+best(X,Y) :- X<Y.
+
+	
+neighbor(L,L2) :-
+	slots(NSlots),
+	nth1(D,L,Slot),
+	between(1,NSlots,NovoSlot),
+	NovoSlot \= Slot,
+	D1 is D-1, length(Prefix,D1),
+	append(Prefix,[Slot|Suffix],L),
+	append(Prefix,[NovoSlot|Suffix],L2).
+		
+		
+%parte de uma soluçao do problema e transita-se sempre que possivel a uma soluçao vizinha (soluçao a partir da atual a partir de um modificador) e assume-se essa soluçao caso seja melhor
+%quando isso deixar de ser possivel (todos os vizinhos forem piores que a soluçao atual) paro. min/max local 		
+hillclimbing(S, LocalOpt) :-
+	f_aval(S,V1),
+	neighbor(S,S2),
+	f_aval(S2,V2),
+	best(V2,V1),
+	!, write(S2:V2),nl, %compromisso: se num espaço de estados se atingir otimo local, nao voltar para tras
+	hillclimbing(S2,LocalOpt).
+	
+hillclimbing(S,S).
+
+	
+
+	
+  
